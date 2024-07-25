9cf43112043c1933abd4a93c1272683b6c763786
rangarao86 <garlapati.sranga@gmail.com>
2024-07-25 10:50:11 +0530

    4th commit

---
+++ webappicicibank/src/main/java/com/icicibank/Two.java
@@ -0,0 +1,13 @@
+package com.icicibank;
+
+/**
+ * Hello world!
+ *
+ */
+public class Two
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
